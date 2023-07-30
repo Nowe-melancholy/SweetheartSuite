1ff783de854a7d1b303d17065b2d772b8dccde6b
Nowe-melancholy <jackpot88230021@gmail.com>
2023-07-30 10:43:38 +0900

    スキーマの自動生成までできた

--- go/pkg/User/migration/migration.go
+++ go/pkg/User/migration/migration.go
@@ -11,6 +11,20 @@ import (
 
 func Init() {
 	fmt.Println("Initializing database...")
+
+	fmt.Println("Create user schema...")
+	db0, err := gorm.Open(postgres.New(postgres.Config{
+		DSN: "host=db user=postgres password=postgres dbname=sweetheartdb port=5432 sslmode=disable TimeZone=Asia/Shanghai",
+	}))
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	db0.Exec("CREATE SCHEMA IF NOT EXISTS \"user\";")
+
+	fmt.Println("Migrating user schema...")
+
 	db, err := gorm.Open(postgres.New(postgres.Config{
 		DSN: "host=db user=postgres password=postgres dbname=sweetheartdb port=5432 sslmode=disable TimeZone=Asia/Shanghai",
 	}), &gorm.Config{
@@ -25,4 +39,6 @@ func Init() {
 	}
 
 	db.AutoMigrate(&infra.User{}, &infra.Couple{})
+
+	fmt.Println("Database initialized")
 }
